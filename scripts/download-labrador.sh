024497568d05ce0b74147a82084b5e235e37be49
Jack Sullivan <jack@divergent.codes>
2023-07-04 21:04:44 -0700

    ignore: rely on config file and dev pulls latest

--- scripts/download-labrador.sh
+++ scripts/download-labrador.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
+# Image is a constant.
 image_repo="ghcr.io/divergentcodes/labrador"
 
-# Determine Docker image tag.
+# If ref is undefined, default to tag=latest.
 if [ -z "$GHACTION_LABRADOR_DOCKER_REF" ]; then
-    echo "No docker image tag defined in env var: GHACTION_LABRADOR_DOCKER_REF"
-    echo "If running in Github Actions, {{ github.action_ref }} might be undefined."
-    exit 1
-fi
+    # If running in Github Actions, {{ github.action_ref }} might be undefined.
+    echo "The Github Action version ref is empty."
+    echo "Using default Docker image tag: latest"
+    GHACTION_LABRADOR_DOCKER_REF="latest"
 
-if echo "$GHACTION_LABRADOR_DOCKER_REF" | grep -qe "^v[0-9]\+"; then
-    echo "Using Docker image $image_repo:$GHACTION_LABRADOR_DOCKER_REF"
+# If ref starts with v[0-9]+, use tag=version.
+elif echo "$GHACTION_LABRADOR_DOCKER_REF" | grep -qe "^v[0-9]\+"; then
+    echo "Using Docker image version tag: $GHACTION_LABRADOR_DOCKER_REF"
+
+# If tag doesn't start with v[0-9], default to tag=latest.
 else
-    echo "Invalid Docker image tag: $image_repo:$GHACTION_LABRADOR_DOCKER_REF"
-    exit 1
+    echo "The Github Action version ref is not a version tag: $GHACTION_LABRADOR_DOCKER_REF"
+    echo "Using default Docker image tag: latest"
+    GHACTION_LABRADOR_DOCKER_REF="latest"
 fi
 
 # Pull and extract Labrador.
-image_name="ghcr.io/divergentcodes/labrador:$GHACTION_LABRADOR_DOCKER_REF"
+image_name="$image_repo:$GHACTION_LABRADOR_DOCKER_REF"
+echo "Pulling Docker image $image_name"
 docker pull $image_name
 
 container_id=$(docker create $image_name)
